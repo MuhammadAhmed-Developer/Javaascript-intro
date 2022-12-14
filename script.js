02848f316c75ba37f013f210847b1ad98f531696
Muhammad Ahmed <119342876+MuhammadAhmed-Developer@users.noreply.github.com>
2022-12-14 10:06:43 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+function stylet(){
+document.getElementById('data').style.fontSize='40px';
+document.getElementById('data').style.color='green';
+document.getElementById('data').style.fontSize='40px';
+document.getElementById('data').style.fontWeight='900';
+document.getElementById('data').style.border='5px solid red';
+document.getElementById('data').style.borderRadius='100px';
+document.getElementById('data').style.textAlign='center';
+document.getElementById('data').innerHTML='HELLOW javascript';
+}
+function addcontent(){
+    document.getElementById('goal').innerHTML="<p style='font-size:40px; color:red; text-align:center; margin: 30px'>I'am a boy</p>"
+}
+addcontent();
+
+// alert('Please enter your email👿😈😈')
+
+console.log('at line 15')
+
+function printdocument(){
+    document.getElementById('bulb').style.display='none';
+    document.getElementById('myImage').style.display='none';
+    document.getElementById('show').style.display='none';
+    
+
+    print();
+}
+function clickalert(){
+    alert('Hyyy Budy')
+    alert('second alert')
+}
